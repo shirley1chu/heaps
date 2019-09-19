137c58ef58c7268a85421abb11b9d20048cf2fdb
Chu <ejpz@M-C02YN16UJG5J.nordstrom.net>
2019-09-19 15:02:28 -0700

    acheived O(1) space complexity

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,44 +1,24 @@
 require_relative "min_heap"
-require "pry"
 
 # This method uses a heap to sort an array.
 # Time Complexity:  O(nlogn)
-# Space Complexity: O(n)
+# Space Complexity: O(1)
 def heapsort(list)
-  # add each element to heap
-  # pop each element back out
   return [] if list == []
   heap = MinHeap.new
-  # heap.store = list
 
-  # list.each_with_index do |elem, i|
-  #   # binding.pry
-  #   elem = HeapNode.new(elem)
-
-  # end
-
-  # list.length.times do
-  #   elem = list.pop
-  #   heap.add(elem)
-  # end
-
-  # list.length.times do
-
-  # binding.pry
-  # list.length.times do
-  #   value = heap.remove
-  #   heap.add(value)
-  # end
-
-  list.each do |elem|
+  until list.empty?
+    # O(1)
+    elem = list.pop
+    # O(nlogn)
     heap.add(elem)
   end
 
-  i = 0
   until heap.empty?
+    # O(nlogn)
     elem = heap.remove
-    list[i] = elem
-    i += 1
+    # O(1)
+    list.push(elem)
   end
 
   return list
